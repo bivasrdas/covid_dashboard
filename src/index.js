6eb11eb15b60817c9c6e0671ea811937605e551a
Bivas <dasbivas321@gmail.com>
2020-09-20 20:03:22 +0530

    changed title,package.json scripts,deleted api.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Speech from './speech'
-import runSample from './api'
+
 
 ReactDOM.render(
   <React.StrictMode>
     {/*<App />*/}
     <Speech/>
-    <runSample/>
   </React.StrictMode>,
   document.getElementById('root')
 );
